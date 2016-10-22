570931c104a9fe027a76f3c662cf7d377a9c5b73
FUYUUNO1-PC\fuyuu.No1 <yu1229kotetu@gmail.com>
2016-10-23 02:16:37 +0900

    マップのあたり判定追加

---
+++ MoniMoniProject/Assets/Scripts/GameMain/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x,
+            player.transform.position.y, -5);
+    }
+}
---
+++ MoniMoniProject/Assets/Scripts/SpriteLoader.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpriteLoader : MonoBehaviour
+{
+    Dictionary<string, Sprite> sprites = new Dictionary<string, Sprite>();
+
+    /**
+     * 読み込み
+     * @param   filepath_
+     * @retval  読み込んだSpriteの数(error -1)
+     */
+    public int Load(string filepath_)
+    {
+        Object[] resources = Resources.LoadAll(filepath_, typeof(Sprite));
+
+        if (resources == null || resources.Length == 0) return -1;
+
+        int len = resources.Length;
+
+        for (int i = 0; i < len; i++)
+        {
+
+            Debug.Log("AddSprite : " + resources[i].name);
+            sprites.Add(resources[i].name, resources[i] as Sprite);
+        }
+
+        return len;
+    }
+
+    /**
+     * 取得
+     * @param   name_   Spriteの名前
+     * @retval  Spriteのインスタンス(なければnull)
+     */
+    public Sprite GetSprite(string name_)
+    {
+        if (!sprites.ContainsKey(name_))
+            return null;
+        return sprites[name];
+    }
+
+}
