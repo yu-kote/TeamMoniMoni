97672cadb40b39b74905306a90c009cbb713013e
EdwardStark <ironmanr335@outlook.jp>
2016-10-27 19:32:30 +0900

    更新しましたー

--- MoniMoniProject/Assets/Hunting/transfrom.cs
+++ MoniMoniProject/Assets/Hunting/transfrom.cs
@@ -7,26 +7,24 @@ public class transfrom : MonoBehaviour
     public float angle = 30f;
     private Vector3 targetPos;
     private Vector3 target;
+    private Vector3 pos;
 
     // Use this for initialization
     void Start()
     {
         Transform target = GameObject.Find("Sample").transform;
         targetPos = target.position;
-        transform.Rotate(new Vector3(0, Random.Range(0, 0), 0), Space.World);
-
+        transform.Rotate(new Vector3(0, Random.Range(0, 360), 0), Space.World);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Mathf.Sin(1);
-        Mathf.Cos(1);
-
-        Vector3 pos = Vector3.one;
         float speed = 2.0f;
 
+        Debug.Log(transform.position);
+        pos = Vector3.zero;
         pos.y += Mathf.Sin(Time.time * speed) * 2f;
         pos.x += Mathf.Cos(Time.time * speed) * 2f;
         target = pos;
