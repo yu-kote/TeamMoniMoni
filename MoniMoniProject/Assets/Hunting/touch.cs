133bfc2fd97522dc01cd17d674a9e648b2834b14
EdwardStark <ironmanr335@outlook.jp>
2016-10-29 21:28:47 +0900

    アニメーションの追加

---
+++ MoniMoniProject/Assets/Hunting/touch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touch : MonoBehaviour
+{
+
+    [SerializeField]
+    private Sprite sprite;
+
+    [SerializeField]
+    private Sprite[] sprites;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	
+	}
+}
