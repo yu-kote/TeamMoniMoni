abfc799516578995ba69cf07caae5d7eb445bb8e
JesterKedama <kedama19961222@gmail.com>
2016-11-14 11:00:34 +0900

    Title変更点 音楽をつけた。

--- MoniMoniProject/Assets/Title/script/TitleWord.cs
+++ MoniMoniProject/Assets/Title/script/TitleWord.cs
@@ -5,11 +5,14 @@ public class TitleWord : MonoBehaviour {
     [SerializeField]
     private int wait_time;
     Vector2 pos;
+    private AudioSource Title_BGM_TAP;
+    private int BGM_start;
     // Use this for initialization
-	void Start () {
+    void Start () {
     pos = transform.localPosition;
-    
-	}
+        BGM_start = 0;
+        Title_BGM_TAP = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,10 +21,12 @@ public class TitleWord : MonoBehaviour {
             wait_time--;
             transform.localPosition = new Vector3(pos.x, pos.y + 150, 0);
         }
-        else
-        {
+        
+        if (wait_time < 0) { wait_time = 0; }
+        if (wait_time == 0) {
+            BGM_start++;
             transform.localPosition = new Vector3(pos.x, pos.y, 0);
         }
-
+        if (BGM_start == 1) Title_BGM_TAP.PlayOneShot(Title_BGM_TAP.clip);
     }
 }
--- MoniMoniProject/Assets/Title/script/fall.cs
+++ MoniMoniProject/Assets/Title/script/fall.cs
@@ -7,9 +7,12 @@ public class fall : MonoBehaviour {
     [SerializeField]
     private float fall_speed;
     Vector3 pos;
+    private AudioSource Title_BGM_Name;
+    private int BGM_start;
     // Use this for initialization
     void Start()
     {
+        Title_BGM_Name = GetComponent<AudioSource>();
 
         pos = transform.localPosition;
         
@@ -24,6 +27,8 @@ public class fall : MonoBehaviour {
             fall_time -= fall_speed;
         }
         if (fall_time < 0) fall_time = 0;
-
+        if (fall_time == 0) BGM_start++;
+        if (BGM_start == 1)Title_BGM_Name.PlayOneShot(Title_BGM_Name.clip);
+       
     }
 }
